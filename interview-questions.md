40c938b46213b5ea0897ab2b1ce71781b4797d45
Franz <franzmarte23@gmail.com>
2023-11-06 08:04:16 -0800

    completes week 5 assessment

--- interview-questions.md
+++ interview-questions.md
@@ -8,32 +8,32 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. Compare and contrast arrays and hashes in Ruby. Include at least two differences and two similarities.
 
-Your answer:
+Your answer: Hash is a collection of key and value pairs. Array is an ordered collection of objects. 
 
-Researched answer:
+Researched answer: A Ruby hash is a dictionary-like container for unique keys and their corresponding values. We can implement operations on a hash using CRUD. Both hash and arrays can be nested. 
 
 2. What does it mean to implement a TTD workflow?
 
-Your answer:
+Your answer: It is how we conduct quality assurance on our code. We only write new code when automated test has failed. 
 
-Researched answer:
+Researched answer: Test-driven development (TDD) is a software development practice that emphasizes writing tests before writing the actual code. It follows a cyclical process of writing a failing test, writing the minimum code to make the test pass, and then refactoring the code.
 
 3. Why would super be used in a Ruby class?
 
-Your answer:
+Your answer: Super is used to call on the initialize method in the superclass. 
 
-Researched answer:
+Researched answer: The super method is used in the subclass. After invoking super the subclass will have access to the instance variables within that method. Instance variables are not inherited since instance variables are local to a specific instance of a class. Via super, you are allowed to borrow instance variables from the parent.
 
 4. STRETCH: What is the difference between inheritance in JavaScript and inheritance in Ruby?
 
-Researched answer:
+Researched answer: Inheritance is a way of reusing code and creating new classes (types of objects) based on existing ones. In Ruby, inheritance allows a class (child class) to inherit (or acquire) the properties and methods of another class (parent class). This helps reduce code duplication and makes it easier to maintain code.
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. PostgreSQL:
+1. PostgreSQL: PostgreSQL, also known as Postgres, is a free and open-source relational database management system emphasizing extensibility and SQL compliance. It was originally named POSTGRES, referring to its origins as a successor to the Ingres database
 
-2. Ruby on Rails:
+2. Ruby on Rails: Ruby on Rails is a server-side web application framework written in Ruby under the MIT License. Rails is a model–view–controller framework, providing default structures for a database, a web service, and web pages.
 
-3. Active Record:
+3. Active Record: The active record is an object that wraps a row in the database table, encapsulates the database access and provides an interface to query, insert, update and delete data from the database, making it easier to work with databases in an application.
--- javascript-challenges.test.js
+++ javascript-challenges.test.js
@@ -14,30 +14,135 @@
 
 // a) Create a test with expect statements using the variables provided.
 
-const secretCodeWord1 = "Lackadaisical"
-// Expected output: "L4ck4d41s1c4l"
-const secretCodeWord2 = "Gobbledygook"
-// Expected output: "G0bbl3dyg00k"
-const secretCodeWord3 = "Eccentric"
-// Expected output: "3cc3ntr1c"
+function encodeString(input) {
+    const encodingMap = {
+      'a': '4',
+      'e': '3',
+      'i': '1',
+      'o': '0',
+    };
+  
+    // Use a regular expression to match and replace the characters
+    return input.replace(/[aeio]/g, (match) => encodingMap[match]);
+  }
+  
+  // Test cases using Jest
+  const secretCodeWord1 = "Lackadaisical";
+  const secretCodeWord2 = "Gobbledygook";
+  const secretCodeWord3 = "Eccentric";
+  
+  test('Encode secretCodeWord1', () => {
+    expect(encodeString(secretCodeWord1)).toBe("L4ck4d41s1c4l");
+  });
+  
+  test('Encode secretCodeWord2', () => {
+    expect(encodeString(secretCodeWord2)).toBe("G0bbl3dyg00l");
+  });
+  
+  test('Encode secretCodeWord3', () => {
+    expect(encodeString(secretCodeWord3)).toBe("3cc3ntr1c");
+  });
+  
+
+// const secretCodeWord1 = "Lackadaisical"
+// // Expected output: "L4ck4d41s1c4l"
+// const secretCodeWord2 = "Gobbledygook"
+// // Expected output: "G0bbl3dyg00k"
+// const secretCodeWord3 = "Eccentric"
+// // Expected output: "3cc3ntr1c"
+
+// // b) Create the function that makes the test pass.
+
+// // Pseudo code:
+
+// Define a function that takes in a string as a parameter
+
+// Create an empty string variable to hold the coded message
+
+// Iterate through each character in the input string
+
+// If the character is "a", replace it with "4" and add it to the coded message
+
+// If the character is "e", replace it with "3" and add it to the coded message
+
+// If the character is "i", replace it with "1" and add it to the coded message
+
+// If the character is "o", replace it with "0" and add it to the coded message
+
+// If the character is not one of the mentioned vowels, add it to the coded message as is
+
+// Return the coded message as the output of the function
+
+// // --------------------2) Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind.
+
+// // a) Create a test with expect statements using the variable provided.
+
+function isFullHouse(hand) {
+    // Create an object to count the occurrences of each number in the hand.
+    const counts = {};
+  
+    // Count the occurrences of each number in the hand.
+    for (const number of hand) {
+      if (counts[number]) {
+        counts[number]++;
+      } else {
+        counts[number] = 1;
+      }
+    }
+  
+    // Check if there is exactly one pair (2 occurrences) and one three of a kind (3 occurrences).
+    const values = Object.values(counts);
+    return values.includes(2) && values.includes(3);
+  }
+
+  test('hand1 is a full house', () => {
+    expect(isFullHouse(hand1)).toBe(true);
+  });
+  
+  test('hand2 is not a full house', () => {
+    expect(isFullHouse(hand2)).toBe(false);
+  });
+  
+  test('hand3 is not a full house', () => {
+    expect(isFullHouse(hand3)).toBe(false);
+  });
+  
+  test('hand4 is a full house', () => {
+    expect(isFullHouse(hand4)).toBe(true);
+  });
+
+// const hand1 = [5, 5, 5, 3, 3]
+// // Expected output: true
+// const hand2 = [5, 5, 3, 3, 4]
+// // Expected output: false
+// const hand3 = [5, 5, 5, 5, 4]
+// // Expected output: false
+// const hand4 = [7, 2, 7, 2, 7]
+// Expected output: true
 
 // b) Create the function that makes the test pass.
 
 // Pseudo code:
+// Define a function that takes in an array as a parameter
 
-// --------------------2) Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind.
+// Check if the length of the array is equal to 5
 
-// a) Create a test with expect statements using the variable provided.
+// Create a counter variable for the number of pairs
 
-const hand1 = [5, 5, 5, 3, 3]
-// Expected output: true
-const hand2 = [5, 5, 3, 3, 4]
-// Expected output: false
-const hand3 = [5, 5, 5, 5, 4]
-// Expected output: false
-const hand4 = [7, 2, 7, 2, 7]
-// Expected output: true
+// Create a counter variable for the number of three-of-a-kinds
 
-// b) Create the function that makes the test pass.
+// Loop through each number in the array
 
-// Pseudo code:
+// Check if the current number appears twice in the array
+
+// If yes, increment the pair counter
+
+// Check if the current number appears three times in the array
+
+// If yes, increment the three-of-a-kind counter
+
+// Check if the pair counter is equal to 1 and the three-of-a-kind counter is equal to 1
+
+// If yes, return true (the array is a full house)
+
+// If no, return false (the array is not a full house)
